9c05e4162176833de19041371ebc2e2355c1f4a1
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2024-03-13 03:23:21 +0200

    Update teacher model

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,11 +2,8 @@ class Teacher < ApplicationRecord
   belongs_to :admin_user, class_name: 'User'
   has_many :appointments
   has_many :availabilities, dependent: :destroy # Add this line
-
   after_initialize :update_active_status
-
   private
-
   def update_active_status
     # Check if the teacher has any appointments for today at the current time
     self.active = !appointments.where('start_time <= ? AND end_time >= ? AND date = ?', Time.current, Time.current,
